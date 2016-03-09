aa7ef891b7b887109b934502eb2baf42e4e2b97b
Thomas LePage <thomaslepage@live.com>
2016-03-09 07:10:14 +0000

    Added article table, model, and validations

---
+++ app/models/article.rb
@@ -0,0 +1,6 @@
+class Article < ActiveRecord::Base
+    
+    # add validations
+    validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+end
