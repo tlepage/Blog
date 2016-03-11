5ea629f83b8f34383ce8bbc0cc1bf010c5d7040e
Thomas LePage <thomaslepage@live.com>
2016-03-11 21:22:46 +0000

    add controller and model tests; began adding integration tests for category

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,19 @@
+class CategoriesController < ApplicationController
+        
+    def index
+        
+    end
+    
+    def new
+        
+    end
+    
+    def show
+
+    end
+    
+    def create
+        
+    end
+
+end
