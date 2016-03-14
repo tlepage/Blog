2b27975a2a61de0df6d2da06739e965c4a996ff3
Thomas LePage <thomaslepage@live.com>
2016-03-14 12:38:05 +0000

    Added integration and functional tests for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,11 @@
 class CategoriesController < ApplicationController
         
     def index
-        
+        @categories = Category.all
     end
     
     def new
-        
+        @category = Category.new
     end
     
     def show
@@ -13,7 +13,18 @@ class CategoriesController < ApplicationController
     end
     
     def create
-        
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:success] = "Category was created successfully"
+            redirect_to categories_path
+        else
+           render 'new'
+        end
     end
+    
+    private
+        def category_params
+            params.require(:category).permit(:name)
+        end
 
 end
